0be6c19b8c7885b70560b3b62f2ce44ce00b5877
Scott Price <sayes2x@gmail.com>
2018-10-11 11:38:49 -0400

    Removed unneeded code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// eslint-disable-next-line
-// import globalStyle from './emotion/globalStyle';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
